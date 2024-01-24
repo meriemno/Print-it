cca79c9d5daee65f699c99f966c097df466f42f9
NORI Meriem <meriem.jebbour@gmail.com>
2024-01-24 12:55:22 +0100

    défilement infini droit et gauche du carrousel

--- assets/script.js
+++ assets/script.js
@@ -18,35 +18,60 @@ const slides = [
 ]
 
 /*****ajout des events listner pour les fleches****/
-let index = 0;
-//recuperer la class img de la fleche gauche
-let eventleft = document.querySelector(".arrow_left");
-//ajout de l'event click sur la fleche gauche
-eventleft.addEventListener("click", () => {
-	index--;
-	updateImage();
-	console.log("fleche gauche")
-})
 
-//recuperer la class img de la fleche droite
-let eventRight = document.querySelector(".arrow_right");
-//ajout de l'event click sur la fleche droite
-eventRight.addEventListener("click", () => {
+		let index = 0;
 
-	index++;
-	updateImage();
-	console.log("fleche droite")
+		//recuperer la class img de la fleche gauche
 
-})
+		let eventleft = document.querySelector(".arrow_left");
+
+		//ajout de l'event click sur la fleche gauche
+
+		eventleft.addEventListener("click", () => {
+			index--;
+			console.log(index);
+			console.log(slides.length);
+
+			//défilement infini gauche du carrousel
+
+			if(index<0){
+				index=slides.length-1;
+			}
+			updateImage();
+			console.log("fleche gauche")
+		})
+
+		//recuperer la class img de la fleche droite
+
+		let eventRight = document.querySelector(".arrow_right");
+
+		//ajout de l'event click sur la fleche droite
+
+		eventRight.addEventListener("click", () => {
+
+			index++;
+			console.log(index);
+			console.log(slides.length);
+
+			//défilement infini droit du carrousel 
+
+			if (index > slides.length - 1) {
+				index = 0;
+			}
+			updateImage();
+			console.log("fleche droite")
+
+		})
 
 
-let bannerDots = document.getElementById("banner");
-let racineDots = document.querySelector(".dots");
+		let bannerDots = document.getElementById("banner");
+		let racineDots = document.querySelector(".dots");
 
-//creer un ul
-let ulDots = document.createElement("ul");
-racineDots.appendChild(ulDots);
-ulDots.classList.add("dots");
+		//creer un ul
+
+		let ulDots = document.createElement("ul");
+		racineDots.appendChild(ulDots);
+		ulDots.classList.add("dots");
 
 
 function createDots() {
@@ -60,6 +85,7 @@ function createDots() {
 		ulDots.appendChild(liDots);
 
 		//affecter le diapositive en cours
+
 		if (i == 0) {
 
 			liDots.classList.add("dot_selected");
@@ -71,15 +97,23 @@ function createDots() {
 
 
 function updateImage() {
+	
 	/**** chnager l'image du slider ******/
 
 	//etape 1 : selectionner la balise img du slider
+
 	let img = document.querySelector(".banner-img");
+
 	//etape 2 : recuperer le nom de la 1 er image du tableau
+
 	let firstImageName = slides[index].image;
+
 	//etape 3 : completer le chemin de l'image
+
 	let srcImg = "assets/images/slideshow/" + firstImageName;
+
 	//etape 4 : modifier attribut src de l'image du slider
+
 	img.src = srcImg;
 
 
@@ -87,10 +121,15 @@ function updateImage() {
 	/**** changer le tagline du slider */
 
 	//etape 1 : selectionner la balise p du slider
+
 	let tagImg=document.querySelector("#banner p");
+
 	//recuperer la structure html du tagline
+
 	let tagTxt=tagImg.innerHTML=slides[index].tagLine;
+
 	//recuperer le tagline sans le span
+
 	tagTxt.textContent=slides[index].tagLine;
 
 
@@ -98,6 +137,7 @@ function updateImage() {
 	/**** changer les dots du slider */
 
 	//etape 1 selectionner la class pour les dots
+
 	let dotLi=document.querySelectorAll(".dot");
 
 	
@@ -106,8 +146,11 @@ function updateImage() {
 		
 		
 		//supprimer la class dot_selected 
+
 		dotLi[i].classList.remove("dot_selected");
+
 		//ajout class dot_selected 
+
 		dotLi[index].classList.add("dot_selected");
 	
 	}
@@ -116,6 +159,7 @@ function updateImage() {
 
 
 function init() {
+
 	createDots();
 	//updateImage();
 
